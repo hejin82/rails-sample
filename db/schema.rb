419ca9941a43a921284b7ddf308f22f79e7c1849
hejin <hejin82@gmail.com>
2018-12-30 02:41:35 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_29_171609) do
+ActiveRecord::Schema.define(version: 2018_12_29_173933) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2018_12_29_171609) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id"
+    t.string "name"
+    t.string "email"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
