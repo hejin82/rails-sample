10a6209cbc3fe8bc97a099eec5f4ba336da6a93c
hejin <hejin82@gmail.com>
2018-12-30 02:24:20 +0900

    modify migrate create_articles_categories

--- db/migrate/20181229171609_create_articles_categories.rb
+++ db/migrate/20181229171609_create_articles_categories.rb
@@ -1,6 +1,11 @@
 class CreateArticlesCategories < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles_categories do |t|
+    create_table :articles_categories, id: false do |t|
+      t.references :article
+      t.references :category
     end
+  end
+  def self.down
+    drop_table :articles_categories
   end
 end
