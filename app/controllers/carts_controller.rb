be3f45831f8b10636ce33d0cfeeb12ce278fd400
hejin <hejin82@gmail.com>
2018-12-29 23:53:29 +0900

    commit

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
   # GET /carts.json
@@ -54,7 +55,8 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
       format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
       format.json { head :no_content }
@@ -70,5 +72,14 @@ class CartsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params.fetch(:cart, {})
+    end
+
+    def line_item_params
+      params.require(:line_item).permit(:product_id)
+    end
+
+    def invalid_cart
+      logger.error("Attempt to access invalid cart #{params[:id]}")
+      redirect_to(store_index_path, notice: "Invalid cart")
     end
 end
